7175eee7ce1f6dba5f3233178490ac680d4df670
JuanPSigcho <juanpsigcho@gmail.com>
2020-10-12 17:01:04 -0500

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    }, 
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+        black: '#000',
+      },
+    }
+  },
+  variants: {},
+  plugins: [],
+}
